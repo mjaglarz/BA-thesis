922f7436121e745eea8478533d5c9f980dbde9d1
Michał Jaglarz <mjaglarz98@gmail.com>
2020-05-10 18:38:58 +0200

    Let's use capital letters for arrays/lists.

--- benchmarks/C/src/quicksort.c
+++ benchmarks/C/src/quicksort.c
@@ -1,28 +1,28 @@
-void quicksort(int *a, int lo, int hi) {
+void quicksort(int *A, int lo, int hi) {
     int i = lo;
     int j = hi;
     while (i < hi) {
-        int pivot = a[(lo + hi) / 2];
+        int pivot = A[(lo + hi) / 2];
         while (i <= j) {
-            while (a[i] < pivot) {
+            while (A[i] < pivot) {
                 i = i + 1;
             }
 
-            while (a[j] > pivot) {
+            while (A[j] > pivot) {
                 j = j - 1;
             }
 
             if (i <= j) {
-                int t = a[i];
-                a[i] = a[j];
-                a[j] = t;
+                int t = A[i];
+                A[i] = A[j];
+                A[j] = t;
                 i = i + 1;
                 j = j - 1;
             }
         }
 
         if (lo < j) {
-            quicksort(a, lo, j);
+            quicksort(A, lo, j);
         }
 
         lo = i;
--- benchmarks/Python/quicksort.py
+++ benchmarks/Python/quicksort.py
@@ -1,23 +1,23 @@
-def quicksort(a, lo, hi):
+def quicksort(A, lo, hi):
     i = lo
     j = hi
     while i < hi:
-        pivot = a[(lo + hi) // 2]
+        pivot = A[(lo + hi) // 2]
         while i <= j:
-            while a[i] < pivot:
+            while A[i] < pivot:
                 i += 1
 
-            while a[j] > pivot:
+            while A[j] > pivot:
                 j -= 1
 
             if i <= j:
-                a[i], a[j] = a[j], a[i]
+                A[i], A[j] = A[j], A[i]
                 i += 1
                 j -= 1
 
         if lo < j:
-            quicksort(a, lo, j)
+            quicksort(A, lo, j)
 
         lo = i
         j = hi
-    return a
+    return A
