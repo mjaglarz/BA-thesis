92079dcb7d3061fd0eb092d80e96f739128abe9c
Michał Jaglarz <mjaglarz98@gmail.com>
2020-05-10 14:45:58 +0200

    Extracted functions for fibonacci and quicksort.

--- benchmarks/C/src/benchmark.c
+++ benchmarks/C/src/benchmark.c
@@ -1,21 +1,14 @@
-#include <assert.h>
 #include <limits.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
 
-#include "fibonacci.h"
-#include "quicksort.h"
 #include "utils.h"
 
 #define NTESTS 100
 #define ARRAYSIZE 1000000
 
-
-int main() {
+int main(int argc, char *argv[]) {
     FILE *fdread;
     FILE *fdwrite;
 
@@ -32,52 +25,17 @@ int main() {
     strcat(read_path, "utils/data/integers.txt");
     strcat(write_path, "results/c_fibonacci_benchmark.txt");
 
-    struct timespec start, end;
-    volatile int fibarg =
-        20; // Each volatile variable read has a corresponding memory access.
-
     fdwrite = fopen(write_path, "w");
-
-    // Test fibonacci function
-    assert(fib(fibarg) == 6765);
-
-    for (int i = 0; i < NTESTS; ++i) {
-        clock_gettime(CLOCK_MONOTONIC, &start);
-        fib(fibarg);
-        clock_gettime(CLOCK_MONOTONIC, &end);
-
-        fprintf(fdwrite, "%f\n", get_elapsed_time(&start, &end));
-    }
-
+    test_fibonacci(fdwrite, 100);
     fclose(fdwrite);
 
-    // Test quicksort function
     write_path[strrchr(write_path, '/') - write_path + 1] = '\0';
     strcat(write_path, "c_quicksort_benchmark.txt");
+
     fdwrite = fopen(write_path, "w");
-
     fdread = fopen(read_path, "r");
-    int *test_array = malloc(ARRAYSIZE * sizeof(int));
-    for (int i = 0; i < ARRAYSIZE; i++) {
-        fscanf(fdread, "%d", &test_array[i]);
-    }
 
-    quicksort(test_array, 0, ARRAYSIZE - 1);
-    assert(check_if_array_is_sorted(test_array));
-    memset(test_array, 0, ARRAYSIZE * sizeof(*test_array));
-
-    for (int i = 0; i < NTESTS; ++i) {
-        rewind(fdread);
-        for (int i = 0; i < ARRAYSIZE; i++) {
-            fscanf(fdread, "%d", &test_array[i]);
-        }
-        clock_gettime(CLOCK_MONOTONIC, &start);
-        quicksort(test_array, 0, ARRAYSIZE - 1);
-        clock_gettime(CLOCK_MONOTONIC, &end);
-        memset(test_array, 0, ARRAYSIZE * sizeof(*test_array));
-
-        fprintf(fdwrite, "%f\n", get_elapsed_time(&start, &end));
-    }
+    test_quicksort(fdwrite, fdread, 100);
 
     fclose(fdread);
     fclose(fdwrite);
--- benchmarks/C/src/utils.c
+++ benchmarks/C/src/utils.c
@@ -1,3 +1,9 @@
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "fibonacci.h"
+#include "quicksort.h"
 #include "utils.h"
 
 #define ARRAYSIZE 1000000
@@ -19,4 +25,48 @@ bool check_if_array_is_sorted(int *array) {
         }
     }
     return true;
+}
+
+void test_fibonacci(FILE *fdwrite, int ntests) {
+    struct timespec start, end;
+    volatile int fibarg = 20;
+
+    assert(fib(fibarg) == 6765);
+
+    for (int i = 0; i < ntests; ++i) {
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        fib(fibarg);
+        clock_gettime(CLOCK_MONOTONIC, &end);
+
+        fprintf(fdwrite, "%f\n", get_elapsed_time(&start, &end));
+    }
+}
+
+void test_quicksort(FILE *fdwrite, FILE *fdread, int ntests) {
+    struct timespec start, end;
+
+    int *test_array = malloc(ARRAYSIZE * sizeof(int));
+    for (int i = 0; i < ARRAYSIZE; i++) {
+        fscanf(fdread, "%d", &test_array[i]);
+    }
+
+    quicksort(test_array, 0, ARRAYSIZE - 1);
+    assert(check_if_array_is_sorted(test_array));
+
+    memset(test_array, 0, ARRAYSIZE * sizeof(*test_array));
+
+    for (int i = 0; i < ntests; ++i) {
+        rewind(fdread);
+        for (int i = 0; i < ARRAYSIZE; i++) {
+            fscanf(fdread, "%d", &test_array[i]);
+        }
+
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        quicksort(test_array, 0, ARRAYSIZE - 1);
+        clock_gettime(CLOCK_MONOTONIC, &end);
+        
+        memset(test_array, 0, ARRAYSIZE * sizeof(*test_array));
+
+        fprintf(fdwrite, "%f\n", get_elapsed_time(&start, &end));
+    }
 }
