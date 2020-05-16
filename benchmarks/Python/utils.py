75b935d29b318487bc4b18534ad4e7040ade3e29
Michał Jaglarz <mjaglarz98@gmail.com>
2020-05-16 18:23:48 +0200

    Added matrix multiplication for python.

--- benchmarks/Python/utils.py
+++ benchmarks/Python/utils.py
@@ -1,8 +1,10 @@
 import os
 from timeit import default_timer as timer  # On Python 3.3+ default_timer is time.perf_counter() on all platforms.
+import numpy as np
 
 import fibonacci
 import quicksort
+import matrix_multiplication as matmul
 
 
 def chop_suffix_from_path(path, suffix):
@@ -31,7 +33,7 @@ def run_test(suite, ntests):
 def test_fibonacci(ntests):
     fibarg = 20
     base_path = chop_suffix_from_path(os.path.dirname(os.path.abspath(__file__)), '/benchmarks/Python')
-    path = base_path + '/results/python_fibonacci_benchmark.txt'
+    path = base_path + '/results/fibonacci/python_fibonacci_benchmark.txt'
 
     assert(fibonacci.fib(fibarg) == 6765)
 
@@ -56,7 +58,7 @@ def test_quicksort(ntests):
         assert(check_if_array_is_sorted(test_array))
         del test_array
 
-        write_path = base_path + '/results/python_quicksort_benchmark.txt'
+        write_path = base_path + '/results/quicksort/python_quicksort_benchmark.txt'
         with open(write_path, 'w') as fdwrite:
             for _ in range(ntests):
                 fdread.seek(0)
@@ -71,7 +73,26 @@ def test_quicksort(ntests):
 
 
 def test_matrix_multiplication(ntests):
-    pass
+    matrix_size = 200
+    base_path = chop_suffix_from_path(os.path.dirname(os.path.abspath(__file__)), '/benchmarks/Python')
+    write_path = base_path + '/results/matrix_multiplication/python_matrix_multiplication_benchmark.txt'
+    read_path = base_path + '/utils/data/matmul_assert.txt'
+
+    A = B = [[row for row in range(matrix_size)] for col in range(matrix_size)]
+    R = [[0] * matrix_size for _ in range(matrix_size)]
+    C = np.matrix(matmul.matmul(A, B, R))
+    D = np.loadtxt(read_path)
+    assert(C.all() == D.all())
+
+    with open(write_path, 'w') as fd:
+        for _ in range(ntests):
+            R = [[0] * matrix_size for _ in range(matrix_size)]
+            start = timer()
+            matmul.matmul(A, B, R)
+            end = timer()
+
+            time = end - start
+            fd.write(f'{time}\n')
 
 
 def test_all(ntests):
