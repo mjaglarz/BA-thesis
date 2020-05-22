ca0418c7d04c0958a6299a71f6138537ab822b34
Michał Jaglarz <mjaglarz98@gmail.com>
2020-05-22 20:24:46 +0200

    Removed dead code.

--- benchmarks/C/include/utils.h
+++ benchmarks/C/include/utils.h
@@ -4,7 +4,6 @@
 #include <time.h>
 
 double get_elapsed_time(struct timespec *start, struct timespec *end);
-int compare(const void *x, const void *y);
 bool check_if_array_is_sorted(int *array);
 bool check_if_arrays_are_equal(size_t size, int A[*][*], int B[*][*]);
 void test_fibonacci(char path[], int ntests);
--- benchmarks/C/src/utils.c
+++ benchmarks/C/src/utils.c
@@ -18,10 +18,6 @@ double get_elapsed_time(struct timespec *start, struct timespec *end) {
     return delta_s + delta_ns * 1e-9;
 }
 
-int compare(const void *x, const void *y) {
-    return (*(double *)x - *(double *)y);
-}
-
 bool check_if_array_is_sorted(int *array) {
     for (int i = 0; i < ASIZE - 1; ++i) {
         if (array[i] > array[i + 1]) {
