2b8623ecfb12eced71b30228298c040beab88ed0
Michał Jaglarz <mjaglarz98@gmail.com>
2020-05-24 20:34:35 +0200

    Updated matrix multiplication results.

--- benchmarks/C/src/matrix_multiplication.c
+++ benchmarks/C/src/matrix_multiplication.c
@@ -3,7 +3,6 @@
 void matmul(int A[][MSIZE], int B[][MSIZE], int R[][MSIZE]) {
     for (int i = 0; i < MSIZE; ++i) {
         for (int j = 0; j < MSIZE; ++j) {
-            R[i][j] = 0;
             for (int k = 0; k < MSIZE; ++k)
                 R[i][j] += A[i][k] * B[k][j];
         }
