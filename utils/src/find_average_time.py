2f285e263eccbf89808d2ae75f0ce7441af5a9f5
Michał Jaglarz <mjaglarz98@gmail.com>
2020-06-01 20:51:05 +0200

    Added program to find average time.

---
+++ utils/src/find_average_time.py
@@ -0,0 +1,24 @@
+import os
+
+import utils
+
+
+def generate_results_path(func, lang):
+    base_path = utils.chop_suffix_from_path(os.path.dirname(os.path.abspath(__file__)), '/utils/src') 
+    return base_path + '/results/' + func + '/' + lang + '_' + func + '_benchmark.txt'
+
+
+def find_average_time(path, func, lang):
+    with open(path, 'r') as fd:
+        array = [float(i) for i in fd.readlines()]
+        array.sort()
+        print(f'{lang.capitalize()} {func.replace("_", " ")} average result: {array[len(array) // 2]:.6f}s.')
+
+
+if __name__ == "__main__":
+    langs = ['c', 'julia', 'python']
+    funcs = ['fibonacci', 'quicksort', 'matrix_multiplication']
+
+    for f in funcs:
+        for l in langs:
+            find_average_time(generate_results_path(f, l), f, l)
