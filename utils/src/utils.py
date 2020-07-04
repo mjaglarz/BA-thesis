506a10527ef9f494635684c312c3284ab05028f5
Michał Jaglarz <mjaglarz98@gmail.com>
2020-07-04 19:01:59 +0200

    Removed alias.

--- utils/src/utils.py
+++ utils/src/utils.py
@@ -1,5 +1,5 @@
 from time import sleep
-from timeit import perf_counter as timer
+from timeit import perf_counter
 
 import numpy.random as np
 
@@ -19,9 +19,9 @@ def gen_integers_array(size, start, end):
 
 def test_sleep(fd, ntests):
     for _ in range(ntests):
-        start = timer()
+        start = perf_counter()
         sleep(100)
-        end = timer()
+        end = perf_counter()
         elapsed_time = end - start
         print(f'Sleep(100): {elapsed_time}')
         fd.write(f'A\t{elapsed_time:.6f}\t1\n')
@@ -31,10 +31,10 @@ def test_sleep(fd, ntests):
 def test_for_sleep(fd, ntests):
     niter = 100
     for _ in range(ntests):
-        start = timer()
+        start = perf_counter()
         for _ in range(niter):
             sleep(1)
-        end = timer()
+        end = perf_counter()
         elapsed_time = end - start
         print(f'For 100 times sleep(1): {elapsed_time}')
         fd.write(f'B\t{elapsed_time:.6f}\t{niter}\n')
@@ -44,10 +44,10 @@ def test_for_sleep(fd, ntests):
 def test_add(fd, ntests, nincr):
     for _ in range(ntests):
         initial_value = val = 0
-        start = timer()
+        start = perf_counter()
         for _ in range(nincr):
             val += 1.0
-        end = timer()
+        end = perf_counter()
         elapsed_time = end - start
         print(f'For {nincr} add 1 to {initial_value}: {elapsed_time}')
         fd.write(f'C\t{elapsed_time:.6f}\t{nincr}\n')
@@ -57,11 +57,11 @@ def test_add(fd, ntests, nincr):
 def test_add_if(fd, ntests, nincr):
     for _ in range(ntests):
         initial_value = val = 0
-        start = timer()
+        start = perf_counter()
         for i in range(nincr):
             if i % 2 == 0:
                 val += 1.0
-        end = timer()
+        end = perf_counter()
         elapsed_time = end - start
         print(f'For {nincr} add 1 to {initial_value}: {elapsed_time}')
         fd.write(f'D\t{elapsed_time:.6f}\t{nincr}\n')
@@ -71,10 +71,10 @@ def test_add_if(fd, ntests, nincr):
 def test_subtract(fd, ntests, nincr):
     for _ in range(ntests):
         initial_value = val = 1000000
-        start = timer()
+        start = perf_counter()
         for _ in range(nincr):
             val -= 1.0
-        end = timer()
+        end = perf_counter()
         elapsed_time = end - start
         print(f'For {nincr} subtract 1 from {initial_value}: {elapsed_time}')
         fd.write(f'E\t{elapsed_time:.6f}\t{nincr}\n')
@@ -84,11 +84,11 @@ def test_subtract(fd, ntests, nincr):
 def test_subtract_if(fd, ntests, nincr):
     for _ in range(ntests):
         initial_value = val = 1000000
-        start = timer()
+        start = perf_counter()
         for i in range(nincr):
             if i % 2 == 0:
                 val -= 1.0
-        end = timer()
+        end = perf_counter()
         elapsed_time = end - start
         print(f'For {nincr} subtract 1 from {initial_value}: {elapsed_time}')
         fd.write(f'F\t{elapsed_time:.6f}\t{nincr}\n')
@@ -98,10 +98,10 @@ def test_subtract_if(fd, ntests, nincr):
 def test_multiply(fd, ntests, nincr):
     for _ in range(ntests):
         initial_value = val = 1
-        start = timer()
+        start = perf_counter()
         for _ in range(nincr):
             val *= 1.01
-        end = timer()
+        end = perf_counter()
         elapsed_time = end - start
         print(f'For {nincr} multiply {initial_value} by 1.01: {elapsed_time}')
         fd.write(f'G\t{elapsed_time:.6f}\t{nincr}\n')
@@ -111,11 +111,11 @@ def test_multiply(fd, ntests, nincr):
 def test_multiply_if(fd, ntests, nincr):
     for _ in range(ntests):
         initial_value = val = 1
-        start = timer()
+        start = perf_counter()
         for i in range(nincr):
             if i % 2 == 0:
                 val *= 1.01
-        end = timer()
+        end = perf_counter()
         elapsed_time = end - start
         print(f'For {nincr} multiply {initial_value} by 1.01: {elapsed_time}')
         fd.write(f'H\t{elapsed_time:.6f}\t{nincr}\n')
@@ -125,10 +125,10 @@ def test_multiply_if(fd, ntests, nincr):
 def test_divide(fd, ntests, nincr):
     for _ in range(ntests):
         initial_value = val = 1000000
-        start = timer()
+        start = perf_counter()
         for _ in range(nincr):
             val /= 1.01
-        end = timer()
+        end = perf_counter()
         elapsed_time = end - start
         print(f'For {nincr} divide {initial_value} by 1.01: {elapsed_time}')
         fd.write(f'I\t{elapsed_time:.6f}\t{nincr}\n')
@@ -138,11 +138,11 @@ def test_divide(fd, ntests, nincr):
 def test_divide_if(fd, ntests, nincr):
     for _ in range(ntests):
         initial_value = val = 1000000
-        start = timer()
+        start = perf_counter()
         for i in range(nincr):
             if i % 2 == 0:
                 val /= 1.01
-        end = timer()
+        end = perf_counter()
         elapsed_time = end - start
         print(f'For {nincr} divide {initial_value} by 1.01: {elapsed_time}')
         fd.write(f'J\t{elapsed_time:.6f}\t{nincr}\n')
