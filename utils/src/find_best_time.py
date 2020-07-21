f6dda77458b2b998250c2e8626a9c5276bf89795
Michał Jaglarz <mjaglarz98@gmail.com>
2020-07-21 15:59:20 +0200

    Min max

--- utils/src/find_best_time.py
+++ utils/src/find_best_time.py
@@ -11,7 +11,7 @@ def generate_results_path(func, lang):
 def find_average_time(path, func, lang):
     with open(path, 'r') as fd:
         array = [float(i) for i in fd.readlines()]
-        print(f'{lang.capitalize()} {func.replace("_", " ")} best result: {max(array):.6f}s.')
+        print(f'{lang.capitalize()} {func.replace("_", " ")} best result: {min(array):.6f}s.')
 
 
 if __name__ == "__main__":
