8c95134033914b6070ce0c045f1e2c24d1868c80
Michał Jaglarz <mjaglarz98@gmail.com>
2020-08-01 21:01:25 +0200

    Now Cython is used correctly

--- benchmarks/Python++/Cython/benchmark.py
+++ benchmarks/Python++/Cython/benchmark.py
@@ -1,10 +1,9 @@
+import pyximport
+pyximport.install(language_level=3, inplace=True)
+
 import sys
 
-import pyximport
-
 import utils
-
-pyximport.install(pyimport = True)
 
 if __name__ == "__main__":
     suite, ntests = sys.argv[1], int(sys.argv[2])
